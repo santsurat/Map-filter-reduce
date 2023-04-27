9d2d279f4fed3a5339fdcd0f9385f53c865bc4e2
@SantSurat <santsurat123@gmail.com>
2023-04-27 22:01:16 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,77 @@
+const arr = [5 , 6 , 8 , 9 , 3 , 2, 4 , 7 , 1];
+
+
+
+// function double(x) {
+//     return x*2;
+// }
+
+//const output = arr.map(triple);
+
+// console.log(output);
+
+
+// function triple(x) {
+//     return x*3;
+// }
+
+//console.log(output);
+
+
+
+// function binary (x)
+// {
+//     return x.toString(2);
+// }
+
+// const output = arr.map(binary);
+
+// console.log(output)
+
+
+// function isEven(x){
+//     return x % 2 === 0 ;
+// }
+
+
+// const output = arr.filter(isEven)
+
+// console.log(output);
+
+// function isOdd(x){
+//     return x % 2 ;
+// }
+
+// const output = arr.filter(isOdd);
+
+// console.log(output);
+
+// function greaterThan5(x){
+//     return x > 5 ;
+// }
+
+const lessThan4 = arr.filter((x) =>  x < 4 );
+
+console.log(lessThan4);
+
+
+const output = arr.map((x) => x*3);
+
+console.log(output);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
